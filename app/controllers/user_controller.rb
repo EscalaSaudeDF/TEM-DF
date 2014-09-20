90b3b30518116082ce342e7d99aab24760b5fceb
Lucas Costa <lucascostaa73@gmail.com>
2014-09-20 13:35:57 -0300

    change rout to home page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,10 @@
 class UserController < ApplicationController
-  def index
-  	render "index"
-  end
+  	def index
+  		render "index"
+  	end
 
-  #save commum user
+  	#save commum user
 	def save_user
-
-
 	end
 
 end
