79ba08c1b3e7ec3c49e7b7d9c3856f380c90586d
= <=>
2014-09-20 09:41:05 -0300

    added class user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ActiveRecord::Base
+	# Attributes
+	def username= (word)
+		@username = word
+	end
+	
+	def email= (word)
+		@email = word
+	end
+
+	def password= (word)
+		@password = word
+	
+	end	
+
+	def documentation= (word)
+		@documentation = word
+	end
+
+	def account_status= (word)
+		@account_status = word
+	end
+
 end
+
+# Instantiates
+user = User.new
