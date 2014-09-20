e5724e8248a9b2b11aa407725cb5c348458e1c56
lugope <lucasgpereira.10@gmail.com>
2014-09-20 14:52:27 -0300

    create action create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   		render "index"
   	end
 
+  	#save common user
+  	def create
+  		@user = User.new params[:username]
+  		@user.save
+  		redirect_to(action: "index", id: @user)
+  	end
+
   	#new common user
   	def new
   		@user = User.new
