c93b3c5d56a41fde39e416132ddab1de01648a43
= <=>
2014-09-22 14:10:57 -0300

    Change variables names - dateInicio to startDate - and - dateFim to endDate

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -7,17 +7,17 @@ class Parser < ActiveRecord::Base
 	def saveMedic()
 		CSV.foreach('csv/escalas.csv', :col_sep => ';', :headers =>true ) do |row|
 
-			dateInicio = DateTime.parse(row["	Data de Inicio da Escala"] + " " + row["	Horario de Entrada"])
-			formatted_dateInicio = dateInicio.strftime('%a %b %d %H:%M')
+			startDate = DateTime.parse(row["	Data de Inicio da Escala"] + " " + row["	Horario de Entrada"])
+			formatted_startDate = startDate.strftime('%a %b %d %H:%M')
 
-			dateFim = DateTime.parse(row["	Data  Final da Escala"] + " " + row["	Horario de Saida"])
-			formatted_dateFim = dateFim.strftime('%a %b %d %H:%M')
+			endDate = DateTime.parse(row["	Data  Final da Escala"] + " " + row["	Horario de Saida"])
+			formatted_endDate = endDate.strftime('%a %b %d %H:%M')
 
         	medic = Medic.find_or_create_by(name: row["	Nome do Servidor"], 
 	        	registry: row["	Matricula Funcional"], speciality: row["Cargo do Servidor"] , 
 	        	department: row["	Descricao da Lotacao"])
 
-        	medic.schedules.build(:in => formatted_dateInicio, :out => formatted_dateFim)
+        	medic.schedules.build(:in => formatted_startDate, :out => formatted_endDate)
 
         	medic.save
 
