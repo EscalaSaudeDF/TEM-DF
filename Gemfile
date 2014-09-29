9111ddeff59cc7319e1bb278fb1deed30f1e68c2
KarineValenca <valenca.karine@gmail.com>
2014-09-29 15:58:28 -0300

    create send email

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'spring',        group: :development
 
 gem "bcrypt", :require => "bcrypt"
 gem 'valid_email'
-gem "paperclip","~> 4.2"
+gem "paperclip", "~> 4.2"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 require 'valid_email'
 class User < ActiveRecord::Base
 
-	has_attached_file :document, styles: {thumbnail: "60x60#"}
 
-	attr_accessor :password_confirmation
+	attr_accessor :password_confirmation, :document
 
 	validates :username, uniqueness: true, presence: true
 	validates :email, uniqueness: true, presence: true, email: true
 	validates :password, presence: true
 	validates :password_confirmation, presence: true
+	
 
 	def self.autenticate (username, password)
 
