ed0e80cdd877e6e213b2f7917fa54f21f4fc63d1
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2014-09-29 13:39:29 -0300

    create method edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,19 @@ class UsersController < ApplicationController
 
   	def create
   		@user = User.new(user_params)
-        if @user.save
-            if @user.password == @user.password_confirmation
-  		          @user.password = BCrypt::Password.create(@user.password)
-                #@user.document = params [:user][:document]
-                @user.save
-                redirect_to(action: "index", id: @user)
-            else
-               render "new" 
-            end
+      if @user.save
+          if @user.password == @user.password_confirmation
+  		        @user.password = BCrypt::Password.create(@user.password)
+              #@user.document = params [:user][:document]
+              @user.save
+              redirect_to(action: "index", id: @user)
+          else
+            render "new" 
+      end
 
-        else
-            render "new"
-        end
+      else
+        render "new"
+      end
   	end
 
     def show
@@ -30,6 +30,24 @@ class UsersController < ApplicationController
   	def new
   		@user = User.new
   	end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.new(user_params)
+      if @user.password != @user.password_confirmation
+        #@user.document = params [:user][:document]
+        render "edit"
+      
+      else
+        @user = User.find(params[:id])
+        params[:user][:password]= BCrypt::Password.create(params[:user][:password])
+        @user.update (user_params)
+        redirect_to(action: "index", id: @user)
+      end
+    end
 
     private
         def user_params
---
+++ app/views/users/edit.html.erb
@@ -0,0 +1,3 @@
+<h1>Editar Usuario</h1>
+
+<%= render 'form' %>
