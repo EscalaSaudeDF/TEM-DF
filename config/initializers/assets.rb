805f4c64aa5777c61fbb2939a55ffd7d7fec4b74
viniband1 <bandeiravinicius0@live.com>
2014-10-01 10:05:52 -0300

    Refining view.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( js/plugins/metisMenu/metisMenu
 Rails.application.config.assets.precompile += %w( js/plugins/morris/raphael.min.js )
 Rails.application.config.assets.precompile += %w( js/plugins/morris/morris.min.js )
 Rails.application.config.assets.precompile += %w( js/plugins/morris/morris-data.js )
+Rails.application.config.assets.precompile += %w( js/sb-admin-2.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
