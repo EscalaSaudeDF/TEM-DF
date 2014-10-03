555dc262d7ae6ca490a879febf688779c26755e4
viniband1 <bandeiravinicius0@live.com>
2014-10-02 23:07:56 -0300

    creating datalist for search.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def index
+		@medic= Medic.all
+		@work_unit= WorkUnit.all
+		list_speciality
+		list_work_unit_name
+	end
+
+	def list_speciality
+
+		@speciality = Array.new
+
+		@medic.each do |medic|
+			unless @speciality.include?(medic.speciality)
+				@speciality.push(medic.speciality)
+			end
+		end
+	end
+	def list_work_unit_name
+		@work_unit_name = Array.new
+
+		@work_unit.each do |work_unit|
+			unless @work_unit_name.include?(work_unit.name_work_unit)
+				@work_unit_name.push(work_unit.name_work_unit)
+			end
+		end
+	end
 end
