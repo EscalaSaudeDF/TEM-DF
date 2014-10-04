0c93831345bd35e847f174fff85ba810c331fffc
Lucas Pereira <lucasgpereira.10@gmail.com>
2014-10-04 16:26:39 -0300

    tests to parser

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -5,24 +5,34 @@ require 'schedule'
 require 'work_unit'
 
 class Parser < ActiveRecord::Base
-	file = 'csv/escalas.csv'
-	def save_parse(file)
+	# file = '/public/csv/schedules.csv'
+	# columns from csv file:
+		# 0 - Nome da Unidade
+		# 1 - Descrição da Lotação
+		# 2 - Matrícula Funcional
+		# 3 - Nome do Servidor
+		# 4 - Cargo do Servidor
+		# 5 - Data de início
+		# 6 - Data de fim
+		# 7 - Horário de entrada
+		# 8 - Horário de saída
+	def self.save_data(file)
 		CSV.foreach(file, :col_sep => ';', :headers =>true ) do |row|
 
-			start_date = DateTime.parse(row["	Data de Inicio da Escala"] + " " + row["	Horario de Entrada"])
+			start_date = DateTime.parse(row[5] + " " + row[7])
 
 			formatted_start_date = start_date.strftime('%a %b %d %H:%M')
 			
-			end_date = DateTime.parse(row["	Data  Final da Escala"] + " " + row["	Horario de Saida"])
+			end_date = DateTime.parse(row[6] + " " + row[8])
 			
 			formatted_end_date = end_date.strftime('%a %b %d %H:%M')
                 	
         	work_unit_record = WorkUnit.find_or_create_by(:name => row[0])
 
-	    	medic_record = work_unit_record.medics.find_or_create_by(:name => row["	Nome do Servidor"],
-				:registry => row["	Matricula Funcional"],
-        		:speciality => row["Cargo do Servidor"],
-        		:department => row["	Descricao da Lotacao"])
+	    	medic_record = work_unit_record.medics.find_or_create_by(:name => row[3],
+				:registry => row[2],
+        		:speciality => row[4],
+        		:department => row[1])
 
         	medic_record.schedules.create(:in => formatted_start_date, 
 	    		:out => formatted_end_date)
--- db/migrate/20140920180724_create_parsers.rb
+++
@@ -1,8 +0,0 @@
-class CreateParsers < ActiveRecord::Migration
-  def change
-    create_table :parsers do |t|
-
-      t.timestamps
-    end
-  end
-end
