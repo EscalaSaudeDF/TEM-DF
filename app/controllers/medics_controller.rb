110daf85a5bf6e0476c8b38fe8f16083d9cd5155
Wesley Araujo <wesley.sickert@gmail.com>
2014-10-09 14:45:20 -0300

    search fixed

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -1,6 +1,7 @@
 class MedicsController < ApplicationController
 	
 	def results
-  		@medics = Medic.search(params[:list_specility])
+  			@medics = Medic.search(params[:list_specility], params[:list_work_unit_name])
+
 	end
 end
--- app/models/medic.rb
+++ app/models/medic.rb
@@ -1,14 +1,24 @@
+require 'work_unit'
+
 class Medic < ActiveRecord::Base
 	belongs_to :work_unit
 	has_many :schedules
 
-		def self.search(search)
-		  if search
-		    #find(:all, :conditions => ['name = ?', "%#{search}%"])
-		    #find_by_speciality(search)
-		    where("speciality =  ?", search).all
-		  else
-		    find(:all)
-		 end
+	def self.search(speciality, work_unit)
+
+		if work_unit != ""
+			@work_unit_instance = WorkUnit.find_by_name(work_unit)
+			
+			if speciality != ""
+				where("speciality =  ? AND work_unit_id = ?", speciality,@work_unit_instance.id).all
+			else
+				where("work_unit_id = ?", @work_unit_instance.id).all
+			end
+			
+		elsif speciality != ""
+			where("speciality =  ?", speciality).all
+		else 
+		    all
+		end
 	end
 end
