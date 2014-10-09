862543c288872bc5d3f8b63903d5cf2233a764a9
lugope <lucasgpereira.10@gmail.com>
2014-10-09 15:30:09 -0300

    Testing update password and 100% test coverage

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -53,9 +53,27 @@ class UsersControllerTest < ActionController::TestCase
 
     test "should edit user's password" do
         session[:remember_token] = @user.id
-
         get :edit_password, id: session[:remember_token]
         assert_response :success
+    end
+
+    test "should update_password" do
+        session[:remember_token] = @user.id 
+        put :update_password, id: session[:remember_token], :user => {:username => "Roberto", :password => "1234", :new_password => "4321", :password_confirmation => "4321"}
+        assert_equal "4321",  assigns(:user).password
+        assert_redirected_to root_path
+    end
+
+    test "shouldn't update_password" do
+        session[:remember_token] = 0
+        put :update_password, id: session[:remember_token]
+        assert_redirected_to edit_password_path
+    end    
+
+    test "shouldn't update_password with wrong confirmation" do
+        session[:remember_token] = @user.id 
+        put :update_password, id: session[:remember_token], :user => {:username => "Roberto", :password => "1234", :new_password => "4321", :password_confirmation => "321"}
+        assert_redirected_to edit_password_path
     end
 
     test "should destroy user" do
