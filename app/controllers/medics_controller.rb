ccfc08f4fb761c334cbcd90c65b2c52bb755a44c
viniband1 <bandeiravinicius0@live.com>
2014-10-10 04:07:21 -0300

    correcting the search.

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -1,7 +1,11 @@
 class MedicsController < ApplicationController
 	
 	def results
+		if params[:list_specility]!= "Informe a Especialidade" || params[:list_work_unit_name]!= "Informe a Região"
   			@medics = Medic.search(params[:list_specility], params[:list_work_unit_name])
-
+  		else
+  			flash.now.alert="Escolha um campo."
+  			render "home/index"
+  		end
 	end
 end
