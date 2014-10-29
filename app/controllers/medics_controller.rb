65b821f0186b6c8b00e6fb0d22b87e67c720c4f5
alexmesquita <alex.mesquita06@hotmail.com>
2014-10-29 10:23:31 -0200

    Decreasing flog complexity in method create_relevance in medics_controller.rb
    Signed-off-by: Álex Mesquita <alex.mesquita06@hotmail.com>
    Signed-off-by: carlosTJunior <carlosjunior117@hotmail.com>

--- app/controllers/medics_controller.rb
+++ app/controllers/medics_controller.rb
@@ -1,5 +1,5 @@
 class MedicsController < ApplicationController
-	
+
 	def results
 		@medics = Medic.search(params[:list_specility], params[:list_work_unit_name])
 		if @medics
@@ -17,10 +17,10 @@ class MedicsController < ApplicationController
 		@ratings = Rating.all.where(medic_id: @medic.id).size
 	end
 
-	def rating 
+	def rating
 		@user = User.find_by_id(session[:remember_token])
 		@medic = Medic.find_by_id(params[:medic_id])
-		
+
 		if @user != nil
 			@rating = Rating.find_by_user_id_and_medic_id(@user.id, @medic.id)
 
@@ -31,16 +31,16 @@ class MedicsController < ApplicationController
 				create_rating(@user, @medic)
 				redirect_to action:"profile",id: params[:medic_id]#,:notice => "O Usuário necessita estar logado"
 			end
-		else 
+		else
 		redirect_to login_path, :notice => "O Usuário necessita estar logado"
-		end	
+		end
   	end
 
   	def create_comment
 		@comment = Comment.new
 		@user = User.find_by_id(session[:remember_token])
 		@medic = Medic.find_by_id(params[:medic_id])
-		
+
 		if @user
 			@comment = Comment.new(content: params[:content], date: Time.now,
 				medic: @medic, user: @user, comment_status: true, report: false)
@@ -68,25 +68,21 @@ class MedicsController < ApplicationController
 
   			if @relevance
   				@relevance.update_attribute(:value, params[:value])
-  				redirect_to profile_path(@medic)
+
   			else
   				@relevance = Relevance.new(value: params[:value], user: @user, comment: @comment)
-	  			
-	  			if @relevance.save
-		        	redirect_to profile_path(@medic)
-		        else
+
+	  			unless @relevance.save
 					flash.now.alert = "Não foi possível avaliar."
-					redirect_to profile_path(@medic)
 				end
-  				redirect_to profile_path(@medic)
   			end
   		else
   			flash.now.alert = "Não foi possível avaliar."
-  			redirect_to profile_path(@medic)
   		end
+		redirect_to profile_path(@medic)
   	end
 
-  	private 
+  	private
   		def create_rating(user, medic)
 			@rating = Rating.new(grade: params[:grade], user: user, medic: medic, date: Time.new)
 			@rating.save
@@ -102,16 +98,15 @@ class MedicsController < ApplicationController
 	  	def calculate_average(medic)
 	  		@ratings = Rating.all.where(medic_id: medic.id)
 
-	  		if @ratings.size == 0 
+	  		if @ratings.size == 0
 	  			return 0
-	  		else 
+	  		else
 		  		sum = 0
 		  		@ratings.each do |r|
-		  			sum += r.grade	
+		  			sum += r.grade
 	  			end
 
 				return sum/(1.0*@ratings.size)
 	  		end
 	  	end
 end
-
