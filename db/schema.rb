55f4c5d8e2fcdd99f6b53c869fbe656f9b4c67bf
lugope <lucasgpereira.10@gmail.com>
2014-11-14 21:09:34 -0200

    Creating link to reset password, and sending email.
        Signed-off-by: Lucas Costa <lucascotaa73@gmail.com>
        Signed-off-by: Lucas Pereira <lucasgpereira.10@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024192938) do
+ActiveRecord::Schema.define(version: 20141114212416) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20141024192938) do
     t.string   "token_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
   create_table "work_units", force: true do |t|
