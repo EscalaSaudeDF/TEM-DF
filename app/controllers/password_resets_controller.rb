525bb876ad2d5c1c2b93f9a4f26688a44e0dad3a
lugope <lucasgpereira.10@gmail.com>
2014-11-15 12:18:39 -0200

    Fixing view and field's validations.
        Signed-off-by: Lucas Costa <lucascotaa73@gmail.com>
        Signed-off-by: Lucas Pereira <lucasgpereira.10@gmail.com>

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,25 +6,31 @@ class PasswordResetsController < ApplicationController
 		user = User.find_by_email(params[:email])
 		if user
 			user.send_password_reset 
-			redirect_to root_url, :notice => "Um e-mail foi mandado com as instrussões para #{:email}."
+			redirect_to root_url, :notice => "Um e-mail foi enviado com as instruções para #{:email}."
 		else
 			redirect_to new_password_reset_path, :notice => "E-mail não existente."
 		end
 	end
 
 	def edit
-		@user = User.find_by_password_reset_token!(params[:id])
+
+		@user = User.find_by_password_reset_token(params[:id])
+		
+		if !@user
+			redirect_to root_path, :notice => "A senha já foi alterada."
+		end
 	end
 
 	def update
 		@user = User.find_by_password_reset_token!(params[:id])
 	
 		if @user.password_reset_sent_at < 2.hours.ago
-			redirect_to new_password_reset_path, :alert => "Password reset has expired."
+			redirect_to new_password_reset_path, :alert => "O link de redefinição de senha expirou."
 		else
 			new_password = params[:user][:password]
 			if new_password == params[:user][:password_confirmation]
 				@user.update_attribute(:password, new_password)
+				@user.update_attribute(:password_reset_token, nil)
 				redirect_to root_url, :notice => "Senha Redefinida!"			
 			else
 				render :edit, flash.now.alert = "Senha e Confirmação não conferem"
