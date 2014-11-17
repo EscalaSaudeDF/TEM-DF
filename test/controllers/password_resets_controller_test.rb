369c89cf31b82ccbe1c9644230713c2844f19821
lugope <lucasgpereira.10@gmail.com>
2014-11-17 15:13:53 -0200

    Testing password_reset and user_mailer
        Signed-off-by: Lucas Costa <lucascotaa73@gmail.com>
        Signed-off-by: Lucas Pereira <lucasgpereira.10@gmail.com>

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,9 +1,53 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+
+	fixtures :users
+
+	def setup
+		@user = users(:roberto)
+		@lorena = users(:lorena)
+	end
+
+	test "should get new" do
+		get :new
+		assert_response :success
+	end
+
+	test "should create" do
+		post :create, email: "roberto@gmail.com", username: "Roberto"
+		assert_redirected_to root_url
+	end
+
+	test "shouldn't create without user" do
+		post :create
+		assert_template "new"
+	end
+
+	test "shouldn't get edit" do
+		get :edit, id: "usuario_inexistente"
+		assert_redirected_to root_url
+	end
+
+	test "should get edit" do
+		get :edit, id: "ToLken123"
+		assert_response :success
+	end
+
+	test "should update" do
+		put :update, id: @user.password_reset_token, :user => {:password => "4321", :password_confirmation => "4321"}
+		assert_redirected_to root_url
+		assert_equal '4321', assigns(:user).password 
+	end
+
+	test "shouldn't update user with expired link" do
+		put :update, id: @lorena.password_reset_token, :user => {:password => "4321", :password_confirmation => "4321"}
+		assert_redirected_to new_password_reset_path
+	end
+
+	test "shouldn't update user with wrong password confirmation" do
+		put :update, id: @user.password_reset_token, :user => {:password => "4A", :password_confirmation => "A4321"}
+		assert_redirected_to edit_password_reset_path
+	end		
 
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+	fixtures :users
+
   test "password_reset" do
-    mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  	@user = users(:roberto)
+    mail = UserMailer.password_reset(@user)
+    assert_equal "Password Reset", mail.subject
+    assert_equal ["roberto@gmail.com"], mail.to
+    assert_equal ["temdf.unb@gmail.com"], mail.from
+    assert_match UserMailer.password_reset(@user).body.encoded, mail.body.encoded
   end
 
 end
